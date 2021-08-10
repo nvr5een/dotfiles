987f92187a0b3076b95dc94f7ccaaeb2c9b36ffb
nvr5een <nvr5een@fakemail.com>
2021-08-09 21:49:22 -0600

    update

--- fish/config.fish
+++ fish/config.fish
@@ -75,3 +75,75 @@ function fish_prompt
 
 end
 
+function :q; exit; end
+function pro; cd ~/projects; end
+
+function grep; grep --color=auto; end
+function ls; ls --color=auto --group-directories-first; end
+function ll; ls -alF; end
+function la; ls -A; end
+function l; ls -CF; end
+
+function df; df -h; end
+function du; du -ch; end
+function mkdir; mkdir -pv; end
+function mount; mount | column -t; end
+
+function chgrp; chgrp --preserve-root; end
+function chmod; chmod --preserve-root; end
+function chown; chown --preserve-root; end
+function cp; cp -iv; end
+function ln; ln -iv; end
+function mv; mv -iv; end
+function rm; rm -Iv; end
+
+function ga; git add; end
+function gb; git branch; end
+function gcm; git commit -v -m; end
+function gco; git checkout; end
+function gcl; git clone; end
+function gd; git diff; end
+function gl; git pull; end
+function gmv; git mv -v; end
+function gp; git push; end
+function grm; git rm; end
+function gs; git status; end
+
+if type -p apt-get &>/dev/null
+  function apti; sudo apt install --no-install-recommends; end
+  function aptu; sudo apt update; end
+  function aptug; sudo apt upgrade; end
+  function aptf; sudo apt full-upgrade; end
+  function aptr; sudo apt remove; end
+  function aptp; sudo apt purge; end
+  function apta; sudo apt autoremove; end
+  function apts; apt search; end
+  function aptq; apt show; end
+  function aptli; apt list --installed | less; end
+  function aptlu; apt list --upgradeable; end
+  function aptstat; dpkg -l | wc -l; end
+end
+
+if type -p pacman &>/dev/null
+  if type -p yay &>/dev/null
+    function paci; yay -S; end
+    function pacu; yay -Syu; end
+    function pacs; yay -Ss; end
+    function pacq; yay -Si; end
+  else
+    function paci; sudo pacman -S; end
+    function pacu; sudo pacman -Syu; end
+    function pacs; pacman -Ss; end
+    function pacq; pacman -Si; end
+  end
+  function pacr; sudo pacman -R; end
+  function pacrr; sudo pacman -Rns; end
+  function pacro; pacman -Qtdq | sudo pacman -Rns -; end
+  function pacli; pacman -Q | less; end
+  function pacll; pacman -Qqm; end
+  function pacf; pacman -Ql; end
+  function pacc; sudo pacman -Sc; end
+  function pacm; makepkg -fsic; end
+  function pacstat; pacman -Q | wc -l; end
+end
+
