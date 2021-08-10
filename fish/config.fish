03fcefb7a2a38ab92a263c7668e21c12e66c1194
nvr5een <nvr5een@fakemail.com>
2021-08-09 22:00:18 -0600

    update

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,5 @@
 # ~/.config/fish/config.fish
 
-set -gx fish_greeting ''
-
 set fish_color_command brgreen
 set fish_color_comment brblack
 set fish_color_end green
@@ -12,90 +10,8 @@ set fish_color_param brblue
 set fish_color_quote yellow
 set fish_color_redirection blue
 
-function hybrid_bindings --description "Vi-style bindings that inherit emacs-style bindings in all modes"
-  for mode in default insert visual
-    fish_default_key_bindings -M $mode
-  end
-  fish_vi_key_bindings --no-erase
-end
-set -g fish_key_bindings hybrid_bindings
-
-function vim_esc_remap --description "Remap 'ESC' to 'jk' for Vi-style bindings"
-  if commandline -P
-    commandline -f cancel
-  else
-    set fish_bind_mode default
-    commandline -f backward-char force-repaint
-  end
-end
-bind -M insert jk vim_esc_remap
-
-set -g __fish_git_prompt_showdirtystate
-set -g __fish_git_prompt_showuntrackedfiles
-
-set -g __fish_git_prompt_char_stateseparator ''
-set -g __fish_git_prompt_color normal
-set -g __fish_git_prompt_color_branch normal
-
-set -g __fish_git_prompt_char_dirtystate '*'
-set -g __fish_git_prompt_char_invalidstate '#'
-set -g __fish_git_prompt_char_stagedstate '+'
-set -g __fish_git_prompt_color_dirtystate yellow
-set -g __fish_git_prompt_color_invalidstate yellow
-set -g __fish_git_prompt_color_stagedstate green
-
-set -g __fish_git_prompt_char_untrackedfiles '%'
-set -g __fish_git_prompt_color_untrackedfiles yellow
-
-function fish_prompt
-
-  set_color blue
-  printf '%s' (prompt_pwd)
-  printf '%s' (__fish_git_prompt)
-
-  if test -z (string match -ri '^no|false|0$')
-    switch $fish_bind_mode
-      case default
-        echo -en "\e[2 q"
-        printf '> '
-      case insert
-        echo -en "\e[6 q"
-        printf '> '
-      case replace_one
-        echo -en "\e[4 q"
-        printf '> '
-      case visual
-        echo -en "\e[2 q"
-        printf '> '
-      case '*'
-        echo -en "\e[2 q"
-        printf '> '
-    end
-  end
-
-end
-
 function :q; exit; end
 function pro; cd ~/projects; end
-
-function grep; grep --color=auto; end
-function ls; ls --color=auto --group-directories-first; end
-function ll; ls -alF; end
-function la; ls -A; end
-function l; ls -CF; end
-
-function df; df -h; end
-function du; du -ch; end
-function mkdir; mkdir -pv; end
-function mount; mount | column -t; end
-
-function chgrp; chgrp --preserve-root; end
-function chmod; chmod --preserve-root; end
-function chown; chown --preserve-root; end
-function cp; cp -iv; end
-function ln; ln -iv; end
-function mv; mv -iv; end
-function rm; rm -Iv; end
 
 function ga; git add; end
 function gb; git branch; end
