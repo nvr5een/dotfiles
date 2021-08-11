88af02f465ff75c10943bda6c8eb9ea2068ae5c8
nvr5een <nvr5een@fakemail.com>
2021-08-10 23:21:41 -0600

    update

--- fish/config.fish
+++ fish/config.fish
@@ -36,7 +36,7 @@ set -g __fish_git_prompt_showdirtystate
 set -g __fish_git_prompt_showuntrackedfiles
 
 set -g __fish_git_prompt_char_stateseparator ''
-set -g __fish_git_prompt_color normal
+set -g __fish_git_prompt_color blue
 set -g __fish_git_prompt_color_branch normal
 
 set -g __fish_git_prompt_char_dirtystate '*'
@@ -58,20 +58,20 @@ function fish_prompt
   if test -z (string match -ri '^no|false|0$')
     switch $fish_bind_mode
       case default
-        echo -en "\e[2 q"
-        printf '> '
+        printf '\e[2 q'
+        printf '$ '
       case insert
-        echo -en "\e[6 q"
-        printf '> '
+        printf '\e[6 q'
+        printf '$ '
       case replace_one
-        echo -en "\e[4 q"
-        printf '> '
+        printf '\e[4 q'
+        printf '$ '
       case visual
-        echo -en "\e[2 q"
-        printf '> '
+        printf '\e[2 q'
+        printf '$ '
       case '*'
-        echo -en "\e[2 q"
-        printf '> '
+        printf '\e[2 q'
+        printf '$ '
     end
   end
 
