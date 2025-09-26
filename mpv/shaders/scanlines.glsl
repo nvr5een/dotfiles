51de712287d175b872c0c8e7f2f7309be1f85dca
nvr5een <nvr5een@protonmail.com>
2025-09-26 17:25:41 -0600

    update

--- mpv/shaders/scanlines.glsl
+++ mpv/shaders/scanlines.glsl
@@ -1,12 +1,9 @@
 //!HOOK MAIN
 //!BIND HOOKED
-//!DESC Scanlines locked to video resolution
+//!DESC Simple scanlines (darken every other line)
 
 vec4 hook() {
-    vec2 uv = HOOKED_pos * HOOKED_size;  // video pixel coords
     vec4 c = HOOKED_tex(HOOKED_pos);
-    if (mod(floor(uv.y), 2.0) == 0.0) {
-        c.rgb *= 0.5;  // adjust strength
-    }
+    if (mod(gl_FragCoord.y, 2.0) < 1.0) c.rgb *= 0.50; // adjust 0.50 for strength
     return c;
 }
